9840f6b92caf7f264487aa32bdb33f3d037b1129
SimonMur <symonmuriuki411@gmail.com>
2023-06-05 14:14:20 +0300

    change db url

--- MechConnectMLAPI/config/db.py
+++ MechConnectMLAPI/config/db.py
@@ -1,3 +1,3 @@
 from pymongo import MongoClient 
-conn = MongoClient("mongodb://simon:mechconnect2023!@38.242.142.213:27017/mechconnect_prompts?authSource=admin")
+conn = MongoClient("mongodb://simon:mechconnect2023!@38.242.142.213:27017/mechconnect_prompts?authSource=admin&retryWrites=false")
 
